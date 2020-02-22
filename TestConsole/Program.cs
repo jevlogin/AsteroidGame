cbde0829cddbbc0329002b8431a82bf674a2b1ae
JEVLOGIN <jev.login@users.noreply.github.com>
2020-02-22 11:24:25 +0300

    Соаздали класс Gamer в тестовой версии

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -6,10 +6,43 @@ using System.Threading.Tasks;
 
 namespace TestConsole
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
+            Gamer gamer = new Gamer();
+            gamer.Name = "JEVLOGIN";
+            gamer.DayOfBirth = new DateTime(1987, 1, 24, 0, 0, 0);
+
+            Gamer[] gamers = new Gamer[100];
+            for (int i = 0; i < gamers.Length; i++)
+            {
+                var g = new Gamer();
+                g.Name = $"Gamer {i + 1}";
+                g.DayOfBirth = DateTime.Now.Subtract(TimeSpan.FromDays(365 * (i + 18)));
+                gamers[i] = g;
+            }
+
+            gamer.SayYourName();
+
+            foreach (var g in gamers)
+            {
+                g.SayYourName();
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+
+    class Gamer
+    {
+        public string Name;
+        public DateTime DayOfBirth;
+
+        public void SayYourName()
+        {
+            Console.WriteLine($"{Name} {DayOfBirth:dd:MM:yyyy HH:mm:ss}");
         }
     }
 }
