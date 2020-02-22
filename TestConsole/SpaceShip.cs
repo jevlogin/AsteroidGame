b99f2f6573aa1138fb11b9ea600ef85e84481a4b
JEVLOGIN <jev.login@users.noreply.github.com>
2020-02-22 22:11:00 +0300

    Создали новый класс Корабль SpaceShip Создали структуру Vector2D

---
+++ TestConsole/SpaceShip.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    class SpaceShip
+    {
+        //  Класс это полнофункциаональный тип данных. Хранится в куче. Ссылочный.
+
+    }
+
+    struct Vector2D
+    {
+        private double _X;
+        private double _Y;
+
+        //  Простое свойство.
+        public double X { get { return _X; } set { _X = value; } }
+        //  Ниже лямбда выражение
+        public double Y { get => _Y; set => _Y = value; }
+        //  А внизу автосвойство. Свойство для ленивых
+        //public double Y { get; set; }
+
+    }
+}
