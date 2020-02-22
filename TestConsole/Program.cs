9547880dac5b9873dbc8f909e1fd9ba073813a37
JEVLOGIN <jev.login@users.noreply.github.com>
2020-02-22 22:25:34 +0300

    Убрали введение в отдельный регион. Закомментировали

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -10,31 +10,36 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
-            Gamer gamer = new Gamer("JEVLOGIN", new DateTime(1987, 1, 24, 0, 0, 0));
+            #region Введение
+            //Gamer gamer = new Gamer("JEVLOGIN", new DateTime(1987, 1, 24, 0, 0, 0));
 
-            Gamer[] gamers = new Gamer[100];
-            for (int i = 0; i < gamers.Length; i++)
-            {
-                var g = new Gamer($"Gamer {i + 1}", DateTime.Now.Subtract(TimeSpan.FromDays(365 * (i + 18))));
-                gamers[i] = g;
-            }
+            //Gamer[] gamers = new Gamer[100];
+            //for (int i = 0; i < gamers.Length; i++)
+            //{
+            //    var g = new Gamer($"Gamer {i + 1}", DateTime.Now.Subtract(TimeSpan.FromDays(365 * (i + 18))));
+            //    gamers[i] = g;
+            //}
 
-            gamer.SayYourName();
+            //gamer.SayYourName();
 
-            foreach (var g in gamers)
-            {
-                g.SayYourName();
-            }
+            //foreach (var g in gamers)
+            //{
+            //    g.SayYourName();
+            //}
 
 
-            //gamer.SetName("Максим");
-            //Console.WriteLine($"{gamer.GetName()}");
+            ////gamer.SetName("Максим");
+            ////Console.WriteLine($"{gamer.GetName()}");
 
-            gamer.Name = "Konstantin";
+            //gamer.Name = "Konstantin";
 
-            //gamer.GetNameLength();
+            ////gamer.GetNameLength();
 
-            Console.WriteLine($"Игрок первый = {gamer.Name}");
+            //Console.WriteLine($"Игрок первый = {gamer.Name}");
+
+            #endregion
+
+
 
             Console.ReadKey();
         }
