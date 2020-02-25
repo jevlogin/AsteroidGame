b39485e18085fc084082e37482692a1949296ff8
JEVLOGIN <jev.login@users.noreply.github.com>
2020-02-25 23:04:48 +0300

    Абстрактный класс графических объектов

--- AsteroidGame/Game.cs
+++ AsteroidGame/Game.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AsteroidGame.VisualObjects;
 
 namespace AsteroidGame
 {
@@ -54,25 +55,53 @@ namespace AsteroidGame
         private static VisualObject[] __GameObjects;
         public static void Load()
         {
-            __GameObjects = new VisualObject[30];
-            for (int i = 0; i < __GameObjects.Length / 3; i++)
+            var game_objects = new List<VisualObject>();
+            var rnd = new Random();
+
+            const int star_count = 100;
+            const int star_size = 5;
+            const int star_max_speed = 20;
+            for (int i = 0; i < star_count; i++)
             {
-                __GameObjects[i] = new VisualObject(new Point(600, i * 20), new Point(15 - i, 20 - i), new Size(20, 20));
+                game_objects.Add(new Star(new Point(rnd.Next(0, Width), 
+                    rnd.Next(0, Height)), new Point(rnd.Next(0, star_max_speed), 0), 
+                    star_size));
             }
 
-            for (int i = __GameObjects.Length / 3; i < __GameObjects.Length *2 / 3; i++)
+            const int ellipse_count = 20;
+            const int ellipse_size_x = 20;
+            const int ellipse_size_y = 30;
+            for (int i = 0; i < ellipse_count; i++)
             {
-                __GameObjects[i] = new Star(new Point(600, i * 10), new Point(i, 20 - i), 20);
+                game_objects.Add(new EllipseObject(new Point(600, i * 20), 
+                    new Point(15 - i, 20 - i), 
+                    new Size(ellipse_size_x, ellipse_size_y)));
             }
 
-            for (int i = __GameObjects.Length * 2 / 3; i < __GameObjects.Length; i++)
-            {
-                __GameObjects[i] = new Cube(new Point(600, i * 10), new Point(i, 20), 20);
-            }
+            #region Это нам не нравится ))
+            //__GameObjects = new VisualObject[30];
+            //for (int i = 0; i < __GameObjects.Length / 3; i++)
+            //{
+            //    //__GameObjects[i] = new VisualObject(new Point(600, i * 20), new Point(15 - i, 20 - i), new Size(20, 20));
+            //}
 
-            var image = Properties.Resources.asteroid;
-            var image_object = new ImageObject(new Point(4, 7), new Point(7, 7), new Size(20, 20), image);
+            //for (int i = __GameObjects.Length / 3; i < __GameObjects.Length *2 / 3; i++)
+            //{
+            //    __GameObjects[i] = new Star(new Point(600, i * 10), new Point(i, 20 - i), 20);
+            //}
 
+            //for (int i = __GameObjects.Length * 2 / 3; i < __GameObjects.Length; i++)
+            //{
+            //    __GameObjects[i] = new Cube(new Point(600, i * 10), new Point(i, 20), 20);
+            //}
+            #endregion
+
+
+
+            //var image = Properties.Resources.asteroid;
+            //var image_object = new ImageObject(new Point(4, 7), new Point(7, 7), new Size(20, 20), image);
+
+            __GameObjects = game_objects.ToArray();
         }
 
         public static void Draw()
--- AsteroidGame/VisualObjects/Cube.cs
+++ AsteroidGame/VisualObjects/Cube.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AsteroidGame
+namespace AsteroidGame.VisualObjects
 {
     class Cube : VisualObject
     {
