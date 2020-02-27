a79d86b190b7ab18c426235d25b95e4c3eaf7d3b
JEVLOGIN <jev.login@users.noreply.github.com>
2020-02-27 19:47:04 +0300

    Сохранили список студентов с оценками

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -16,13 +16,23 @@ namespace TestConsole
 {
     static class Program
     {
+        public static List<int> GetRandomRatings(Random rnd, int CountMin, int CountMax)
+        {
+            var count = rnd.Next(CountMin, CountMax + 1);
+            var result = new List<int>(count);
+            for (int i = 0; i < count; i++)
+            {
+                result.Add(rnd.Next(2, 6));
+            }
+            return result;
+        }
         static void Main(string[] args)
         {
             var dekanat = new Dekanat();
-
+            var rnd = new Random();
             for (int i = 0; i < 100; i++)
             {
-                dekanat.Add(new Student { Name = $"Student {i + 1}", });
+                dekanat.Add(new Student { Name = $"Student {i + 1}", Rating = GetRandomRatings(rnd, 20, 50)});
             }
             const string students_data_file = "students.csv";
             dekanat.SaveToFile(students_data_file);
