19a991c0051459740cd9d1f8f28057a2c6efb29c
JEVLOGIN <jev.login@users.noreply.github.com>
2020-03-02 02:56:42 +0300

    Но основное предназначение Enumerable.Range() другое

--- TestConsole/StudentsTests.cs
+++ TestConsole/StudentsTests.cs
@@ -49,6 +49,15 @@ namespace TestConsole
             IEnumerable<Student> students_enum = dekanat;
             //   поддерживает запросы к себе. Оба интерфейсы.
             IQueryable<Student> students_query = students_enum.AsQueryable();
+
+            foreach (var i in Enumerable.Range(0, 100))
+            {
+            }
+            for (int i = 0; i < 100; i++)
+            {
+            }
+            //  равносильные записи. Но основное предназначение Enumerable.Range() другое
+
         }
     }
 }
