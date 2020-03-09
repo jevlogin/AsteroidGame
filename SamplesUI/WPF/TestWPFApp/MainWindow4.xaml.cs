d8df19d04a3e87e1494eac1b2915e955630fe0c1
JEVLOGIN <jev.login@users.noreply.github.com>
2020-03-10 00:56:23 +0300

    Поиск ресурсов

---
+++ SamplesUI/WPF/TestWPFApp/MainWindow4.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TestWPFApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow4.xaml
+    /// </summary>
+    public partial class MainWindow4 : Window
+    {
+        public MainWindow4()
+        {
+            InitializeComponent();
+        }
+        private void btnClickMe_Click(object sender, RoutedEventArgs e)
+        {
+            //lbResult.Items.Add(pnlMain.FindResource("strPanel").ToString());
+            //lbResult.Items.Add(this.FindResource("strWindow").ToString());
+            //lbResult.Items.Add(Application.Current.FindResource("strApp").ToString());
+            //  В принципе, поиск ресурсов в предыдущем примере можно выполнять всегда на уровне панели. 
+            //  Пока не ясно почему..
+            lbResult.Items.Add(pnlMain.FindResource("strPanel").ToString());
+            lbResult.Items.Add(pnlMain.FindResource("strWindow").ToString());
+            lbResult.Items.Add(pnlMain.FindResource("strApp").ToString());
+
+        }
+    }
+}
