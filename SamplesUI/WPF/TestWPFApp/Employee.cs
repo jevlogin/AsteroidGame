6e26a58786bd2c6e9f22f673cf2cebc9fd19934f
JEVLOGIN <jev.login@users.noreply.github.com>
2020-03-10 21:07:17 +0300

    Создаем сущности

---
+++ SamplesUI/WPF/TestWPFApp/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace TestWPFApp
+{
+    internal class Employee
+    {
+        public int Id { get; internal set; }
+        public string Name { get; internal set; }
+        public int Age { get; internal set; }
+        public int Salary { get; internal set; }
+    }
+}
--- SamplesUI/WPF/TestWPFApp/MainWindow7.xaml.cs
+++ SamplesUI/WPF/TestWPFApp/MainWindow7.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +20,29 @@ namespace TestWPFApp
     /// </summary>
     public partial class MainWindow7 : Window
     {
+        ObservableCollection<Employee> items = new ObservableCollection<Employee>();
         public MainWindow7()
         {
             InitializeComponent();
+            FillList();
+        }
+
+        private void FillList()
+        {
+            items.Add(new Employee() { Id = 1, Name = "Константин", Age = 28, Salary = 3000 });
+            items.Add(new Employee() { Id = 1, Name = "Максим", Age = 26, Salary = 3000 });
+            items.Add(new Employee() { Id = 1, Name = "Олег", Age = 30, Salary = 3000 });
+            LbEmployee.ItemsSource = items;
+        }
+
+        private void LbEmployee_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
