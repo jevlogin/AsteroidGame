1eddd13286ef10b364f8a343c860fb36b32bfa12
JEVLOGIN <jev.login@users.noreply.github.com>
2020-03-14 14:15:29 +0300

    Конвертер Visibility

--- BindingTest/BindingTestApp/MainWindow2.xaml.cs
+++ BindingTest/BindingTestApp/MainWindow2.xaml.cs
@@ -15,7 +15,7 @@ namespace BindingTestApp
             horizontal_binding.Path = new PropertyPath("Value");
             HorizontalValue.SetBinding(TextBlock.TextProperty, horizontal_binding);
 
-            var vertical_binding = new Binding();
+            var vertical_binding = new Binding();   //  создаем новую привязку?
             vertical_binding.ElementName = "VerticalSlider";
             vertical_binding.Path = new PropertyPath("Value");
             VerticalValue.SetBinding(TextBlock.TextProperty, vertical_binding);
