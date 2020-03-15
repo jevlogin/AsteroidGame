77f1e3d323a4bb23ff6ce62c5cc49a207e4f7cf3
JEVLOGIN <jev.login@users.noreply.github.com>
2020-03-15 16:54:36 +0300

    Заголовок окна проекта MVVM

---
+++ MVVMTesting/MVVMTestApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMTestApp.ViewModels
+{
+    class MainWindowViewModel
+    {
+        public string Title { get; set; } = "Заголовок окна проекта MVVM";
+    }
+}
