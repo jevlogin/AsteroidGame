a46882f90bf2a73ab06e0b28d161f619e566df89
JEVLOGIN <jev.login@users.noreply.github.com>
2020-03-15 16:45:12 +0300

    пример работы со списочными данными MVVM

--- EmployeesManager/Models/Employee.cs
+++ EmployeesManager/Models/Employee.cs
@@ -1,19 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace EmployeesManager.Models
 {
-    class Employee
+    class Employee : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _Name;
+
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Name
+        {
+            get => _Name;
+            set
+            {
+                _Name = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Name)));
+            }
+        }
         public string SurName { get; set; }
         public string Patronymic { get; set; }
         public DateTime DayOfBirth { get; set; }
         public int Age => (int)Math.Floor((DateTime.Now - DayOfBirth).TotalDays / 365);
+
 
         public override string ToString()
         {
