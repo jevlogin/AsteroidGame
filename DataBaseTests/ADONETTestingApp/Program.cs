606592448e55e4408d85c66682aada610b6b67ff
JEVLOGIN <jev.login@users.noreply.github.com>
2020-03-16 05:41:20 +0300

    Строка подключения к базе данных.

--- DataBaseTests/ADONETTestingApp/Program.cs
+++ DataBaseTests/ADONETTestingApp/Program.cs
@@ -4,6 +4,14 @@
     {
         static void Main(string[] args)
         {
+            var connection_string = @"Data Source = (localdb)\MSSQLLocalDB;"
+                                        + "Initial Catalog = EmployeesDB;"
+                                        + "Integrated Security = True;"
+                                        + "Connect Timeout = 30;"
+                                        + "Encrypt = False;"
+                                        + "TrustServerCertificate = False;"
+                                        + "ApplicationIntent = ReadWrite;"
+                                        + "MultiSubnetFailover = False";
         }
     }
 }
