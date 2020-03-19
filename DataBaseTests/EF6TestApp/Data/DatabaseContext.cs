9f3cdf195ddcbad63366bfef433ce88cfb89b661
JEVLOGIN <jev.login@users.noreply.github.com>
2020-03-19 19:08:35 +0300

    ВСе можно пользоваться. На большем уровне чем мы более часа колупались с ADO.NET

--- DataBaseTests/EF6TestApp/Data/DatabaseContext.cs
+++ DataBaseTests/EF6TestApp/Data/DatabaseContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EF6TestApp.Entities;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -9,6 +10,8 @@ namespace EF6TestApp.Data
 {
     class DatabaseContext : DbContext
     {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Departament> Departaments { get; set; }
         public DatabaseContext() : this("DefaultConnection")
         {
 
