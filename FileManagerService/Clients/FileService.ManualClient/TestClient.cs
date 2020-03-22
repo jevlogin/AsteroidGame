20a18310daf757399da5663945e4ef0dc0346bb5
JEVLOGIN <jev.login@users.noreply.github.com>
2020-03-22 13:18:01 +0300

    пример клиента-сервера web-api

---
+++ FileManagerService/Clients/FileService.ManualClient/TestClient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ServiceModel;
+using FileManagerService;
+using System.IO;
+using System.ServiceModel.Channels;
+
+namespace FileService.ManualClient
+{
+    class TestClient : ClientBase<IFileService>, IFileService
+    {
+        public TestClient(Binding binding, EndpointAddress endpoint) : base(binding, endpoint)
+        {
+
+        }
+        public DirectoryInfo[] GetDirectories(string Path) => Channel.GetDirectories(Path);
+
+        public DriveInfo[] GetDrives() => Channel.GetDrives();
+
+        public FileInfo[] GetFiles(string Path) => Channel.GetFiles(Path);
+
+        public DateTime GetServiceTime() => Channel.GetServiceTime();
+    }
+}
---
+++ FileManagerService/WebAPI/WebAPIHost/Controllers/EmployeesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPIHost.Data;
+using WebAPIHost.Models;
+
+namespace WebAPIHost.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        public IEnumerable<Employee> Get() => TestData.Employees;
+    }
+}
