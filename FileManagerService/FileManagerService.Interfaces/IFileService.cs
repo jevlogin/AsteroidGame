d548dbaf97e3707a14baee7ace51570a251bcaed
JEVLOGIN <jev.login@users.noreply.github.com>
2020-03-22 04:54:28 +0300

    Отдельный интерфейс. Сервис контракты. и Операторы контрактов

---
+++ FileManagerService/FileManagerService.Interfaces/IFileService.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.ServiceModel;
+
+namespace FileManagerService
+{
+    [ServiceContract]
+    public interface IFileService
+    {
+        [OperationContract]
+        DriveInfo[] GetDrives();
+
+        [OperationContract]
+        FileInfo[] GetFiles(string Path);
+
+        [OperationContract]
+        DirectoryInfo[] GetDirectories(string Path);
+    }
+}
